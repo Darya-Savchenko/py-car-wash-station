0df569b55f9a73c3311f7d887d446c240f7cbf5a
Darya-Savchenko <savchenkodarya277@gmail.com>
2024-12-12 19:38:51 +0200

    fix AI suggestions

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 class Car:
-    def __init__(self, comfort_class: str, clean_mark: str, brand: str)\
-            -> None:
+    def __init__(self, comfort_class: int, clean_mark: int, brand: str) -> \
+            None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
@@ -14,16 +14,16 @@ class CarWashStation:
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
-    def calculate_washing_price(self, car: str) -> None:
+    def calculate_washing_price(self, car: Car) -> None:
         return round((car.comfort_class * (self.clean_power - car.clean_mark)
                       * self.average_rating)
                      / self.distance_from_city_center, 1)
 
-    def wash_single_car(self, car: str) -> None:
+    def wash_single_car(self, car: Car) -> None:
         if car.clean_mark < self.clean_power:
             car.clean_mark = self.clean_power
 
-    def serve_cars(self, cars: str) -> None:
+    def serve_cars(self, cars: Car) -> None:
         income = 0
         for car in cars:
             if car.clean_mark < self.clean_power:
